4b7fd75cd7d4bb65e6d3a3df28e3b8a38711da05
tvenkatakautilya <tvenkatakautilya@gmail.com>
2024-06-28 12:05:00 +0530

    Check out functionality

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart.service';
 import { Product } from '../models/products.models';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Subject, map, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-cart',
@@ -11,11 +14,21 @@ export class CartComponent implements OnInit {
   cartItems: Product[] = [];
   isCollapsed: boolean = true;
   totalPrice: number = 0;
+  private apiUrl = environment.apiUrl;
+  private ngUnsubscribe = new Subject();
+  message = '';
+  errorMessage = '';
+  
 
-  constructor(public cartService: CartService) {}
+  constructor(public cartService: CartService, private http: HttpClient,) {}
 
   ngOnInit(): void {
     this.updateCart();
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next(null);
+    this.ngUnsubscribe.complete();
   }
 
   updateCart(): void {
@@ -34,5 +47,24 @@ export class CartComponent implements OnInit {
 
   toggleCollapsed(): void {
     this.isCollapsed = !this.isCollapsed;
+  }
+
+  placeOrder(): void {
+    const requestdata = this.cartItems.map(({ id, quantity }) => ({ product: id, quantity }));
+     
+    console.log(requestdata)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    this.http.post<any>(`${this.apiUrl}/orders/`, {"items": requestdata}).pipe(
+      takeUntil(this.ngUnsubscribe),
+      map(response => {
+        console.log(response);
+        this.message = "Order created succesfully"
+      })
+    ).subscribe({
+      error: error => {
+        this.errorMessage = "Unable to place order, please retry after some time"
+        console.error('Error placing order:', error);
+      }
+    });
   }
 }
